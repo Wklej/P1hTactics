aa04ffbb1271b31b5ca1d2733156cdcebc0b9161
Filip Wawrzyniak <kontogje@gmail.com>
2025-03-30 20:18:54 +0200

    no need for api key refresher since using Developer Api Key

--- src/main/java/com/p1h/p1htactics/service/ApiKeyRefresherService.java
+++ src/main/java/com/p1h/p1htactics/service/ApiKeyRefresherService.java
@@ -1,19 +1,19 @@
-package com.p1h.p1htactics.service;
-
-import com.p1h.p1htactics.config.ApiKeyProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class ApiKeyRefresherService {
-    private final ApiKeyProvider apiKeyProvider;
-
-    @Scheduled(fixedRate = 1 * 60 * 1000)
-    public void refreshApiKey() {
-        System.out.println("Refreshing API Key...");
-        System.out.println("New API Key: " + apiKeyProvider.getApiKey());
-    }
-
-}
+//package com.p1h.p1htactics.service;
+//
+//import com.p1h.p1htactics.config.ApiKeyProvider;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class ApiKeyRefresherService {
+//    private final ApiKeyProvider apiKeyProvider;
+//
+//    @Scheduled(fixedRate = 1 * 60 * 1000)
+//    public void refreshApiKey() {
+//        System.out.println("Refreshing API Key...");
+//        System.out.println("New API Key: " + apiKeyProvider.getApiKey());
+//    }
+//
+//}
