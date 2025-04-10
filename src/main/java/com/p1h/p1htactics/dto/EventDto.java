7de66905ba96799f9d18255235e0cf5320c9ac3e
Filip Wawrzyniak <kontogje@gmail.com>
2025-04-10 22:50:43 +0200

    Events refactoring (remove unnecessary class)

--- src/main/java/com/p1h/p1htactics/dto/EventDto.java
+++
@@ -1,9 +0,0 @@
-package com.p1h.p1htactics.dto;
-
-import java.time.LocalDate;
-import java.util.List;
-
-public record EventDto(String title,
-                       LocalDate start,
-                       LocalDate end,
-                       List<String> participants) {}
